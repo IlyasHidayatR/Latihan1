bbce5a72bd85cc74ddb785e3f12b52c73bafb7f7
Ilyas Hidayat Rusdy <79189496+IlyasHidayatR@users.noreply.github.com>
2021-03-02 14:36:07 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <link rel="icon" type="image/png" href="media/food.png">
+    <title>Ayo Makan!!!</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
+    <link rel="connical" herf="http://makanan.com">
+    <meta property="og::locale" content="en_us">
+    <meta property="og::type" content="website">
+    <meta property="og::title" content="">
+    <meta property="og::description" content="">
+    <meta property="og::url" content="">
+    <meta property="og::site_name" content="">
+
+    <style type="text/css">
+    .left    { text-align: left;}
+    .right   { text-align: right;}
+    .center  { text-align: center;}
+    .justify { text-align: justify;}
+    </style>
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="navbar" style="background-image:url('assets/Bg.jpg'); margin: 0px">
+                <div class="pull-left">
+                    <img src="media/spoon.png" width="100">
+                </div>
+                <div class="pull-left">
+                    <h2>Selamat Datang</h2>
+                    <h4>Portal Makanan Dunia</h4>
+                </div>
+            </div>
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">About</a>
+                    </div>
+                    <div class="collapse navbar-collapse" id=myNavbar>
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#">Home</a></li>
+                            <li class="dropdown">
+                                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Jenis Makanan<span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Asia</a></li>
+                                    <li><a href="#">Eropa</a></li>
+                                    <li><a href="#">Amerika</a></li>
+                                    <li><a href="#">Afrika</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Request</a></li>
+                            <li><a href="#">Help</a></li>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="form.php"><span class="glyphicon glyphicon-user"></span>Sign Up</a></li>
+                            <li><a href="#"><span class="glyphicon gliphicon-log-in"></span>Login</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div class="content">
+                <div class="col-lg-8">
+                    <div class='conteudo'>
+                        <h1>Rendang</h1>
+                        <hr>
+                        <div class="post-info">
+                            Di posting oleh <b>Admin</b>
+                        </div>
+                        <img style="float:left; width:250px; margin:10px;" src="media/rendang.jpg">
+                        <p class="justify">
+                        Rendang atau randang (Jawi: رندڠ) adalah masakan daging yang berasal dari Minangkabau. Masakan ini dihasilkan 
+                        dari proses memasak suhu rendah dalam waktu lama menggunakan aneka rempah-rempah dan santan. 
+                        Proses memasaknya memakan waktu berjam-jam (biasanya sekitar empat jam) hingga yang tinggal hanyalah potongan 
+                        daging berwarna hitam pekat dan dedak. Dalam suhu ruangan, rendang dapat bertahan hingga berminggu-minggu. 
+                        Rendang yang dimasak dalam waktu yang lebih singkat dan santannya belum mengering disebut kalio, berwarna cokelat terang keemasan.
+                        </p>
+                        <a href="#" class="continue-lendo">Selengkapnya-></a>
+                    </div>
+                    <hr>
+                    <div class="conteudo">
+                        <h1>Masakkan Rendang</h1>
+                        <hr>
+                        <div class="post-info">
+                            Di posting oleh <b>Admin</b>
+                        </div>
+                        <img style="float:left; width:250px; margin:10px;" src="media/rendang.jpg">
+                        <p class="justify">
+                        Rendang dapat dijumpai di Rumah Makan Padang di seluruh dunia. Masakan ini populer di Indonesia dan negara-negara Asia Tenggara lainnya, seperti Malaysia, Singapura, 
+                        Brunei, Filipina dan Thailand. Di daerah asalnya, Minangkabau, rendang disajikan di berbagai upacara adat dan perhelatan istimewa. 
+                        Meskipun rendang merupakan masakan tradisional Minangkabau, teknik memasak serta pilihan dan penggunaan bumbu rendang berbeda-berda menurut daerah.
+                        Pada 2011, rendang dinobatkan sebagai hidangan yang menduduki peringkat pertama daftar World's 50 Most Delicious Foods (50 Hidangan Terlezat Dunia) versi CNN International. 
+                        Pada 2018, rendang secara resmi ditetapkan sebagai salah satu dari lima hidangan nasional Indonesia. Rendang dimanfaat sebagai bantuan pangan bagi korban bencana alam karena 
+                        tahan lama dan kandungan gizinya, seperti pada gempa bumi Lombok 2018, gempa bumi dan tsunami Sulawesi 2018, tsunami Selat Sunda 2018, dan banjir Bengkulu 2019.
+                        Bumbu rendang diolah secara fusion untuk makanan seperti spageti, mi, burger, hingga susyi.
+                        </p>
+                        <a href="#" class="conttinue-lendo">Selengkapnya-></a>
+                    </div>
+                    <hr>
+                </div>
+                <div class="col-lg-4" style="background-color:aliceblue">
+                    <div class="widget">
+                        <h2>Makanan Populer</h2>
+                        <ul class=menusidebar>
+                            <li><a href="">Makanan Nusantara</a></li>
+                            <li><a href="">Makanan Western</a></li>
+                            <li><a href="">Makanan Jepang</a></li>
+                        </ul>
+                    </div>
+                    <div class="widget">
+                        <h2>Resep Masakkan</h2>
+                        <ul class=menusidebar>
+                            <li><a href="">Masakkan Nusantara</a></li>
+                            <li><a href="">Masakkan Luar Nusantara</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="myModal" class="modal fade" role="dialog">
+            <div class="modal-dialog">
+                <!--modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&time;</button>
+                        <h4 class="modal-tittle">Berhasil</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p></p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
